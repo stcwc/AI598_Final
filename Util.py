e9c5164715755d3214b241cd5f2f050ae1f9b246
SHENG <john0707ieem@gmail.com>
2017-04-13 21:41:35 -0400

    from sheng. parser mainly

--- Util.py
+++ Util.py
@@ -1,6 +1,4 @@
 
-
-
 class graph:
     def __init__(self):
 
--- test.py
+++ test.py
@@ -1,3 +1,16 @@
+
+class p:
+    def __init__(self,a):
+        self.a=1
+
+    def __str__(self):
+        return ("321")
 class test:
     def __init__(self,a):
         self.a=a
+        self.b = p(2)
+    def __str__(self):
+        return (str)(self.b)
+
+t=test("asdfsadf")
+print(t)
