fee35a361173ccb735028d81a590e168360c389e
YI <monkeyyi0922@gmail.com>
2017-04-13 21:34:51 -0400

    13/04/10pm

--- Util.py
+++ Util.py
@@ -1,25 +1,69 @@
+#defin the class of Point, Graph, Obstacle, Environment
+#Author: YI
+
+class Point:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def __str__(self):
+        return ("("+str(self.x)+","+str(self.y)+")")
+        
+class Graph:
+    def __init__(self,p):
+        #p is the start point
+        self.VertexIndex={p:0}
+        self.IndexVertex={0:p}
+        self.edges=[]
+        self.vertexNumber=1
+
+    def __str__(self):
+        return ("vertex:")  #to be contonue
+
+    def addVertex(self,p):
+        self.vertexIndex[p]=self.VertexNumber
+        self.IndexVertex[self.VertexNumber]=p
+        self.VertexNumber=self.VertexNumber+1
+
+    def addEdge(self,edge):
+        if ( (edge[0] in self.IndexVertex) and (edge[1] in self.IndexVertex) ):
+            self.edges.append(edge)
+        else:
+            print("The vertices are not in the graph!")
+            sys.exit(1)
+
+    def removeEdge(self,edge):
+        if edge in self.edges:
+            self.edges.remove(edge)
+
+    def getVertex(self,index):
+        return self.IndexVertex[index]
+
+    def getIndex(self,point):
+        return self.VertexIndex[point]
+
+
+class Obstacle:
+    def __init__(self,points):
+        self.VertexIndex={}
+        self.IndexVertex={}
+        for i in range(len(points)):
+            self.VertexIndex[points[i]]=i+1
+            self.IndexVertex[i+1]=points[i]
+        self.vertexNumber=len(points)    
+
+    def getVertex(self,index):
+        return self.IndexVertex[index]
+
+    def getIndex(self,point):
+        return self.VertexIndex[point]
 
 
 
-class graph:
-    def __init__(self):
+class Environment:
+    def __init__(self,x,y):
+        self.obstacles=[]
+        self.x_max=x
+        self.y_max=y
 
-    def addVertex(self):
-
-    def addEdge(self):
-
-    def removeVertex(self):
-
-    def removeEdge(self):
-
-
-class obstacle:
-    def __init__(self):
-
-    def addObstacle(self):
-
-
-class environment:
-    def __init__(self):
-
-    def addObstacle(self):
+    def addObstacle(self,obstacle):
+        self.obstacles.append(obstacle)
