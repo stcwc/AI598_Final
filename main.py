63eaed4d4756b0d731066a7cb596fc95ed24e777
SHENG <john0707ieem@gmail.com>
2017-04-20 14:10:33 -0400

    little modification

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ import sys
 
 def main():
     random.seed(1)
-    if len(sys.argv)==3 and sys.argv[2]=="VCD":
+    if len(sys.argv)==4 and sys.argv[3]=="VCD":
         E = parse(sys.argv[1])
         G = Graph(E.start)
         print("......Doing VCD......")
         VCD(E,G)
         path=A_star(G, 0, 1)
-    elif len(sys.argv)==4 and sys.argv[2]=="RRT":
+    elif len(sys.argv)==5 and sys.argv[3]=="RRT":
         E = parse(sys.argv[1])
         G = Graph(E.start)
-        print("......Doing RRT with "+str(int(sys.argv[3]))+" iterations......")
-        I=int(sys.argv[3])
+        I=int(sys.argv[4])
+        print("......Doing RRT with "+str(I)+" iterations......")
         RRT(E,G,I)
         path=A_star(G, 0, G.VertexNumber-1)
     else:    
-        print("[ERROR] Input arguments incorrect. Use command like 'python main.py input.txt VCD' or 'python main.py input.txt RRT 1000' and try again.")
+        print("[ERROR] Input arguments incorrect. Use command like 'python main.py input.txt output.txt VCD' or 'python main.py input.txt output.txt RRT 1000' and try again.")
         sys.exit()
         
     #print("\n\n")
@@ -29,7 +29,7 @@ def main():
     for i in path:
         spath += str(i)+","
     #print(str(G)+"\n"+spath[:-1])
-    file = open("output.txt","w")
+    file = open(sys.argv[2],"w")
     file.truncate()
     file.write(str(G)+"\n"+spath[:-1])
     print("[Success] Output has been written into 'output.txt'.")
