82e9f184271472a2d4fc2560c3ff077e5537e392
YI <monkeyyi0922@gmail.com>
2017-04-20 11:35:29 -0400

    20/04/2017

--- test_main.py
+++ test_main.py
@@ -2,25 +2,30 @@
 from Util import Point, Graph, Obstacle, Environment
 
 
-p=Point(1,2)
-b=Point(1.0,2.0)
-print(p==b)
+a=Point(1,2)
+b=Point(3,4)
+aa=Point(11,2)
+aaa=Point(1,2)
 
+dis={a:'a',b:'b'}
+print(dis)
+for i in dis:
+    print(str(i))
 
-g=Graph(p)
-#g.addVertex(b)
-g.addVertex(Point(4,6))
-g.addVertex(Point(3,7))
-g.addEdge((0,1))
-print(str(g))
-print(str(g.getVertex(3)))
-print(str(g.getIndex(Point(3,9))))
+    
+for i in dis:
+    i.x=i.x+10
 
-o=Obstacle([Point(1,1),Point(2,1),Point(2,2)])
-print(str(o))
-print(str(o.getVertex(4)))
-print(str(o.getIndex(Point(3,9))))
+for i in dis:
+    print(str(i))
+    if i in dis:
+        print("equal")
 
+print(a in dis.keys())
+print(aa in dis.keys())
+print(aaa in dis.keys())
+
+print(str(a))
 #e=Environment(300,200)
 #e.addObstacle(o)
 #print(str(e))
